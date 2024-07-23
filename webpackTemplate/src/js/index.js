9e1f6329bfc2b4cc1773eff14f02cb98cf82bde5
Kirill <vek7434625@gmail.com>
2024-07-23 16:39:32 +0500

    Remove "if" to foreach

--- webpackTemplate/src/js/index.js
+++ webpackTemplate/src/js/index.js
@@ -8,25 +8,25 @@ import SimpleBar from 'simplebar';
 
 
 function moveSwitcher(event) {
-    if (!(event.target.classList.contains("left-block-title--unselected") || event.target.classList.contains("right-block-title--unselected") || event.target.classList.contains("switch__bar-container"))) {
+    if (!(event.target.classList.contains("switcher__left-block-title--unselected") || event.target.classList.contains("switcher__right-block-title--unselected") || event.target.classList.contains("switch__bar-container"))) {
         return 0;
     }
     let left, right, bar, leftContent, rightContent;
     if (event.target.classList.contains("switch__bar-container")) {
-        left = event.target.parentElement.parentElement.querySelectorAll(".left-block-title");
-        right = event.target.parentElement.parentElement.querySelectorAll(".right-block-title");
+        left = event.target.parentElement.parentElement.querySelectorAll(".switcher__left-block-title");
+        right = event.target.parentElement.parentElement.querySelectorAll(".switcher__right-block-title");
         bar = event.target.querySelector(".switch__bar");
-        leftContent = event.target.parentElement.parentElement.parentElement.querySelectorAll(".left-block-content");
-        rightContent = event.target.parentElement.parentElement.parentElement.querySelectorAll(".right-block-content");
+        leftContent = event.target.parentElement.parentElement.parentElement.querySelectorAll(".switcher__left-block-content");
+        rightContent = event.target.parentElement.parentElement.parentElement.querySelectorAll(".switcher__right-block-content");
     } else {
-        left = event.target.parentElement.querySelectorAll(".left-block-title");
-        right = event.target.parentElement.querySelectorAll(".right-block-title");
+        left = event.target.parentElement.querySelectorAll(".switcher__left-block-title");
+        right = event.target.parentElement.querySelectorAll(".switcher__right-block-title");
         bar = event.target.parentElement.querySelector(".switch__bar");
-        leftContent = event.target.parentElement.parentElement.querySelectorAll(".left-block-content");
-        rightContent = event.target.parentElement.parentElement.querySelectorAll(".right-block-content");
+        leftContent = event.target.parentElement.parentElement.querySelectorAll(".switcher__left-block-content");
+        rightContent = event.target.parentElement.parentElement.querySelectorAll(".switcher__right-block-content");
     }
-    left.forEach(x => x.classList.toggle("left-block-title--unselected"));
-    right.forEach(x => x.classList.toggle("right-block-title--unselected"));
+    left.forEach(x => x.classList.toggle("switcher__left-block-title--unselected"));
+    right.forEach(x => x.classList.toggle("switcher__right-block-title--unselected"));
     bar.classList.toggle("switch__bar--right");
     if (window.innerWidth < 780) {
         if (bar.classList.contains("switch__bar--right")) {
@@ -35,15 +35,15 @@ function moveSwitcher(event) {
             bar.style.width = left[0].clientWidth ? `${left[0].clientWidth}px` : `${left[1].clientWidth}px`;
         }
     }
-    leftContent.forEach(x => x.classList.toggle("left-block-content--hidden"));
-    rightContent.forEach(x => x.classList.toggle("right-block-content--hidden"));
+    leftContent.forEach(x => x.classList.toggle("switcher__left-block-content--hidden"));
+    rightContent.forEach(x => x.classList.toggle("switcher__right-block-content--hidden"));
 }
 
 
 function scaleSwitcherBar() {
     if (window.innerWidth < 780) {
-        let left = document.querySelectorAll(".switcher .left-block-title");
-        let right = document.querySelectorAll(".switcher .right-block-title");
+        let left = document.querySelectorAll("switcher__left-block-title");
+        let right = document.querySelectorAll("switcher__right-block-title");
         let bar = document.querySelectorAll(".switch__bar");
         bar.forEach(x => {
             if (x.classList.contains("switch__bar--right")) {
@@ -86,13 +86,13 @@ function choiceArticle(event) {
 }
 
 const switchBarContainer = document.querySelectorAll(".switch__bar-container");
-if (switchBarContainer) switchBarContainer.forEach(x => x.addEventListener("click", moveSwitcher));
+switchBarContainer.forEach(x => x.addEventListener("click", moveSwitcher));
 const articlesCategListingContentLabel = document.querySelectorAll(".articles-categ-listing__content label");
-if (articlesCategListingContentLabel) articlesCategListingContentLabel.forEach(x => {x.addEventListener("click", choiceArticle)});
+articlesCategListingContentLabel.forEach(x => {x.addEventListener("click", choiceArticle)});
 document.addEventListener("click", toggleArticleCategList)
 document.addEventListener("DOMContentLoaded", scaleSwitcherBar);
 window.addEventListener("resize", scaleSwitcherBar);
-const switcherLeftBlockTitle = document.querySelectorAll(".switcher .left-block-title");
-if (switcherLeftBlockTitle) switcherLeftBlockTitle.forEach(x => x.addEventListener("click", moveSwitcher));
-const switcherRightBlockTitle = document.querySelectorAll(".switcher .right-block-title");
-if (switcherRightBlockTitle) switcherRightBlockTitle.forEach(x => x.addEventListener("click", moveSwitcher));
+const switcherLeftBlockTitle = document.querySelectorAll(".switcher__left-block-title");
+switcherLeftBlockTitle.forEach(x => x.addEventListener("click", moveSwitcher));
+const switcherRightBlockTitle = document.querySelectorAll(".switcher__right-block-title");
+switcherRightBlockTitle.forEach(x => x.addEventListener("click", moveSwitcher));
