7d8c5d4a697834d21a3d9895cb971259ccb53f6c
Kirill <vek7434625@gmail.com>
2024-08-30 14:15:51 +0500

    Fix select

--- webpackTemplate/src/js/inputs.js
+++ webpackTemplate/src/js/inputs.js
@@ -1,7 +1,32 @@
 import SimpleBar from 'simplebar';
 
+function closeInput(event) {
+    closeSelectList(event);
+}
+
+function closeSelectList(event) {
+    let select = event.target.closest(".select");
+    if (!openedList) {
+        return;
+    }
+    if (!select) {
+        openedList.classList.remove("select--list");
+        openedList = null;
+    }
+}
+let openedList = null;
 function openSelectList() {
-    this.parentElement.classList.toggle("select--list");
+    if (this.parentElement.classList.contains("select--list")) {
+        this.parentElement.classList.remove("select--list");
+        openedList = null;
+    } else if (openedList) {
+        openedList.classList.remove("select--list");
+        this.parentElement.classList.add("select--list");
+        openedList = this.parentElement;
+    } else {
+        this.parentElement.classList.add("select--list");
+        openedList = this.parentElement;
+    }
 }
 function choiceSelectItem(event, selectEl) {
     selectEl.classList.remove("select--list");
@@ -10,7 +35,7 @@ function choiceSelectItem(event, selectEl) {
     selectEl.querySelector(".select__input").setAttribute("value", event.target.getAttribute("data-value"));
     selectEl.querySelector(".select__label").textContent = event.target.textContent;
 }
-
+document.addEventListener("click", closeInput);
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".select").forEach(x => {
         new SimpleBar(x.querySelector(".select__list"), {autoHide: false});
